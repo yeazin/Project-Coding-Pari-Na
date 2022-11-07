9fadbab8e2cf29e23d41e8e817c02068d35b6f52
yeazin <naz.yeasin@gmail.com>
2022-11-07 22:05:32 +0600

    added login view and registerview

--- structure/accounts/models/profile.py
+++ structure/accounts/models/profile.py
@@ -2,3 +2,29 @@
 This file created for Profile setup 
 '''
 
+from django.db import models
+
+from mainConfig.models.mixin import TimeStampMixin
+
+
+## Profile Class 
+
+class Profile(TimeStampMixin):
+    user = models.ForeignKey(
+        "accounts.User",on_delete= models.CASCADE,null=True,
+        related_name='profile'
+    )
+    full_name = models.CharField(
+        max_length=400,null=True
+    )
+    phone = models.CharField(
+        max_length=11,null=True,unique=True
+    )
+
+    def __str__(self):
+        return str(self.full_name)
+
+    class Meta:
+        verbose_name_plural = "Profile"
+        indexes = [models.Index(fields=['id','user','phone'])]
+
--- structure/codelist/admin.py
+++ structure/codelist/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from structure.codelist.models import ListInput
 
-# Register your models here.
+
+admin.site.register(ListInput)
