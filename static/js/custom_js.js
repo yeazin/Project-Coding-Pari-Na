16dc4a23ef11b75503478dea4244524942c3c137
Md. N. Islam Yeasin <naz.yeasin@gmail.com>
2022-11-11 15:10:40 +0600

    Bug fixed in JS file

--- static/js/custom_js.js
+++ static/js/custom_js.js
@@ -30,7 +30,6 @@ function add_value_to_storage(){
 // Functions for checking given input in list input fields 
 
 function list_given_checker(){
-
   let check_input = document.getElementById('list_checker').value.replaceAll(' ', '');
   const list_input_format = /^[0-9 ,]+$/g;
   const double_comma = /(,{2,})/g;
@@ -38,23 +37,30 @@ function list_given_checker(){
 
   if (!list_input_format.test(check_input)){
       // checking if given input has anyting except Number
+
+      document.getElementById("khoj_submit").disabled = true;
       document.getElementById('error_show').style.color = "red";
       document.getElementById('error_show').innerHTML = "Please Type Number only";
 
   }else if(double_comma.test(check_input)){
       // checking if given input has double comma in it
+      document.getElementById("khoj_submit").disabled = true;
       document.getElementById('error_show').style.color = "red";
       document.getElementById('error_show').innerHTML = "Please return a list with single seprated comma.For example ( 23,32,545 )";
       
   }else if (check_input[0] == ',' || check_input.slice(-1) == ','){
       // checking if given input has comman in first or last character
+      document.getElementById("khoj_submit").disabled = true;
       document.getElementById('error_show').style.color = "red";
       document.getElementById('error_show').innerHTML = "Please Remove First or Last Comma";
       
   }else{
       // if above condition pass then return given input removing all space 
+      document.getElementById("khoj_submit").disabled = false;
       document.getElementById('error_show').style.color = "green";
       document.getElementById('error_show').innerHTML = "Looking Good !!";
       document.getElementById('list_checker').value = check_input;
+      
+
   }
 }
